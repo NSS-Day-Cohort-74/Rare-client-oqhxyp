3cedc71ba0c3e2acd8a521dd691d85f90d568262
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-06 11:41:34 -0500

    PostDetails Module Started, commiting changes before rebasing with Develop

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,48 +1,33 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPostById } from "../../services/postServices";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getPostById } from "../../services/postServices";
 
 export const PostDetails = ({token}) => {
-  const [post, setPost] = useState()
+  const [post, setPost] = useState([])
   const [currentUser, setCurrentUser] = useState()
+  const [category, setCategory] = useState([])
+  const [tags, setTags] = useState([])
+  const [reactions, setReactions] = useState([])
   const {postId} = useParams()
 
   const fetchAndSetPostData = () => {
-    getPostById(Number(postId)).then((postDataArray) => {
+    getPostById(postId).then((postDataArray) => {
       setPost(postDataArray)
     })
   }
 
   useEffect(() => {
     fetchAndSetPostData()
-  },[])
-  
-  console.log(post)
+  },[current])
 
-  if(postId && post){
-    return <>
-      <section>
-        <h1>{post.title}</h1>
-        <section>
-            <div>
-              Delete and Settings Placeholder
-            </div>
-            <div>
-              {post.categories.label}
-            </div>
-        </section>
-        <img src={post.image_url} alt="description here"></img>
-        <section>
-          <div>
-            Published on {post.publication_date} by {post.user.first_name} {post.user.last_name}
-          </div>
-          <div>
-            <button>View Comments</button>
-          </div>
-        </section>
-        <section>
-          {post.content}
-        </section>
-      </section>
-    </>
-  }}
+    return (
+        <>
+
+      <div>Post Details</div>
+        
+        </>
+    );
+  };
