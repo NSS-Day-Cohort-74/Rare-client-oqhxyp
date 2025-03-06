24f7c317d77eb716622af97afa8316406618a3ed
Kai Swain <kaiferseain@icloud.com>
2025-03-06 10:01:10 -0600

    i got all tags populating on the client view

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+import { getAllTags } from "../../services/tagsService";
+
 export const AllTags = () => {
+  const [allTags, setAllTags] = useState([])
+
+  const fillAllTags = () => {
+    getAllTags()
+    .then((data) => setAllTags(data))
+    console.log(allTags)
+  }
+
+  useEffect(() => {
+    fillAllTags()
+  },[])
+
     return (
         <>
 
       <div>AllTags</div>
-        
+          {allTags.map(t => ( 
+            <div key={t.id}> {t.label} </div>
+          ))}
         </>
     );
   };
---
+++ src/views/ApplicationViews.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Register } from "../components/auth/Register"
+import { Authorized } from "./Authorized"
+
+export const ApplicationViews = ({ token, setToken }) => {
+  /*
+  
+  const [token, setTokenState] = useState(localStorage.getItem('auth_token'))
+  
+  const setToken = (newToken) => {
+    localStorage.setItem('auth_token', newToken)
+    setTokenState(newToken)
+    } */
+  
+  return <>
+    
+  </>
+}
+
