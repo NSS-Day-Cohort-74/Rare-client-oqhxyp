ce92d670269a0328fbbbd4567c192166c39b01cc
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-06 13:41:08 -0500

    finised the initial post-details requirements outlined in ticket #5

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -6,28 +6,48 @@ import { useParams } from "react-router-dom";
 import { getPostById } from "../../services/postServices";
 
 export const PostDetails = ({token}) => {
-  const [post, setPost] = useState([])
+  const [post, setPost] = useState()
   const [currentUser, setCurrentUser] = useState()
-  const [category, setCategory] = useState([])
-  const [tags, setTags] = useState([])
-  const [reactions, setReactions] = useState([])
   const {postId} = useParams()
 
   const fetchAndSetPostData = () => {
-    getPostById(postId).then((postDataArray) => {
+    getPostById(Number(postId)).then((postDataArray) => {
       setPost(postDataArray)
     })
   }
 
   useEffect(() => {
     fetchAndSetPostData()
-  },[current])
+  },[])
+  
+  console.log(post)
 
-    return (
-        <>
+  if(postId && post){
+    return <>
+      <section>
+        <h1>{post.title}</h1>
+        <section>
+            <div>
+              Delete and Settings Placeholder
+            </div>
+            <div>
+              {post.categories.label}
+            </div>
+        </section>
+        <img src={post.image_url} alt="description here"></img>
+        <section>
+          <div>
+            Published on {post.publication_date} by {post.user.first_name} {post.user.last_name}
+          </div>
+          <div>
+            <button>View Comments</button>
+          </div>
+        </section>
+        <section>
+          {post.content}
+        </section>
+      </section>
+    </>
+  
 
-      <div>Post Details</div>
-        
-        </>
-    );
-  };
+  }}
