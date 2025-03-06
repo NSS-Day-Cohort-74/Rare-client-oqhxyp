0d461d63f7f42377c9d6822efdb90ee285570b51
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-06 17:21:09 -0500

    in progress module, not finalized.

--- src/components/posts/CreatePost.js
+++ src/components/posts/CreatePost.js
@@ -1,15 +1,19 @@
 import { useEffect, useRef, useState } from "react";
-
 import { getTags } from "../../services/tagServices";
 import { createNewPost } from "../../services/postServices";
 import { getCategories } from "../../services/categoriesService";
+import { HumanDate } from "../utils/HumanDate";
+import { useNavigate } from "react-router-dom";
 
 export const NewPost = ({token}) => {
     const title=useRef()
     const imageUrl=useRef()
     const content=useRef()
+    const [selectedCategory, setSelectedCategory] = useState([])
+    const [selectedTags, setSelectedTags] = useState([])
     const [categories, setCategories]=useState([])
     const [tags, setTags]=useState([])
+    const navigate=useNavigate()
 
     useEffect(()=>{
         getCategories().then((categoriesArray)=>{
@@ -20,14 +24,54 @@ export const NewPost = ({token}) => {
         });
     },[])
 
+    const handleTagsChange = (e) => {
+      const foundTag = selectedTags.find(selectedTags => {
+          return selectedTags.id === Number(e.target.id)
+      })
+      if(foundTag){
+        console.log(foundTag)
+        const newCurrentTags = selectedTags.filter(selectedTag => {
+          return selectedTag.id !== foundTag.id
+        })
+        setSelectedTags(newCurrentTags)
+      }else{
+        const newTag = {
+          tagId:Number(e.target.id)
+        }
+        const addedTags = [...selectedTags, newTag]
+        setSelectedTags(addedTags)
+      }
+    }
+
     const handleSavePost=async(event)=>{
         event.preventDefault()
+
+        const date= new Date()
+        const editedDate=HumanDate(date)
+        
         const createdPost={
+          user_id: token,
+          category_id: selectedCategory,
+          title: title.current.value,
+          publication_date: editedDate,
+          image_url: imageUrl.current.value,
+          content: content.current.value,
+          approved: 1
+    }
+    console.log(date)
+    const NewPost=await createNewPost(createdPost)
+    if (selectedTags.length > 0 && NewPost.id){
+      const tagsInPostArray = selectedCategory.map((tag) =>({
+        post_id: NewPost.id,
+        tag_id: tag.tagId,
+      }))
+      await createTagInPost(tagsInPostArray)
     }
     
-    createNewPost(createdPost)}
-
-
+  
+  }
+    
+  console.log(selectedTags)
     return (
         <section className="columns is-centered">
           <form className="column is-two-thirds" onSubmit={handleSavePost}>
@@ -50,7 +94,13 @@ export const NewPost = ({token}) => {
             <div className="field">
               <label className="label">Category</label>
               <div className="control">
-                <input className="input" type="text" ref={content} />
+                <select className="input" onChange={(e) => setSelectedCategory(e.target.value)}>
+                    {categories.map(category => (
+                      <option value={category.id} key={category.id}>
+                        {category.label}
+                      </option>
+                    ))}
+                </select>
               </div>
             </div>
 
@@ -58,6 +108,23 @@ export const NewPost = ({token}) => {
               <label className="label">Content</label>
               <div className="control">
                 <input className="input" type="text" ref={content} />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Tags</label>
+              <div className="control">
+                   {tags.map(tag => {
+                      return ( 
+                        <div key={tag.id}>
+                          <input
+                            type="checkbox"
+                            id={tag.id}
+                            onChange={handleTagsChange}
+                          />{tag.label}
+                        </div>
+                      )
+                   })}
               </div>
             </div>
     
