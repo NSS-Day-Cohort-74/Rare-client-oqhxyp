7483f249eb9a91233f834132a96a231be9331bde
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-06 11:41:34 -0500

    PostDetails Module Started, commiting changes before rebasing with Develop

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getPostById } from "../../services/postServices";
 
-export const PostDetails = () => {
+export const PostDetails = ({token}) => {
+  const [post, setPost] = useState([])
+  const [currentUser, setCurrentUser] = useState()
+  const [category, setCategory] = useState([])
+  const [tags, setTags] = useState([])
+  const [reactions, setReactions] = useState([])
+  const {postId} = useParams()
+
+  const fetchAndSetPostData = () => {
+    getPostById(postId).then((postDataArray) => {
+      setPost(postDataArray)
+    })
+  }
+
+  useEffect(() => {
+    fetchAndSetPostData()
+  },[current])
+
     return (
         <>
 
