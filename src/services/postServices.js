c3f490849e4e48c33ab555e19a03e752fd2babae
Edita Adomaityte <adomaityteedita02@gmail.com>
2025-03-06 13:27:02 -0600

    Start of createPost functionality

--- src/services/postServices.js
+++ src/services/postServices.js
@@ -1,3 +1,13 @@
 export const getPostById = (postId) => {
     return fetch (`http://localhost:8088/posts/${postId}`).then(res => res.json())
 }
+export const createNewPost =async(newPostObj)=>{
+    const response=await fetch("http://localhost:8088/posts",{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(newPostObj)
+    })
+    return response.json()
+}
