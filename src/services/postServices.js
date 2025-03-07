41a63ff29fae5dcd54abcfc97fa223c5b0e2a003
John Bailey Williams <johnbaileywilliams96@gmail.com>
2025-03-07 11:38:52 -0600

    added to cat and added to update post

--- src/services/postServices.js
+++ src/services/postServices.js
@@ -9,3 +9,18 @@ export const getPostById = (postId) => {
     return fetch (`http://localhost:8088/posts/${postId}`).then(res => res.json())
 }
 
+export const updatePost = async (postId, updatedData) => {
+    const response = await fetch(`http://localhost:8088/posts/${postId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedData),
+    });
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    
+    return response.json();
+  };
