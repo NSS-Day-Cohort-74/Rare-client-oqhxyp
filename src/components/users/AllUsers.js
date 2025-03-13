ded3ccd310fd375cd42dff4c580383895574e0a5
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-03-13 10:33:18 -0400

    Bugfix for allUsers

--- src/components/users/AllUsers.js
+++ src/components/users/AllUsers.js
@@ -2,9 +2,7 @@ import { useState, useEffect } from "react"
 import { AuthorUsers } from "./AuthorUsers"
 import { getUserById } from "../../services/userServices"
 import { AdminUsers } from "./AdminUsers"
-import { useEffect, useState } from "react"
-import { getUsers } from "../../services/userServices"
-import { Link } from "react-router-dom"
+
 
 export const AllUsers = ({token}) => {
     const [currentUser, setCurrentUser] = useState()
@@ -31,3 +29,4 @@ export const AllUsers = ({token}) => {
             </>
         }
     }
+}
