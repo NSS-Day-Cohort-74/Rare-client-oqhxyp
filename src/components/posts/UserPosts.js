beb4fd11fb05719a6eadb380aa451c0d27ad6d50
Edita Adomaityte <adomaityteedita02@gmail.com>
2025-03-14 10:48:49 -0500

    implemented HumanDate

--- src/components/posts/UserPosts.js
+++ src/components/posts/UserPosts.js
@@ -2,21 +2,21 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { deletePost, getPosts } from "../../services/postServices";
 import { deletePostTags } from "../../services/tagServices";
+import { HumanDate } from "../utils/HumanDate";
 
 export const UserPosts = ({ token }) => {
   const navigate = useNavigate();
   const [allPosts, setPosts] = useState([]);
   const [myPosts, setMyPosts] = useState([]);
-  
+
   useEffect(() => {
-    console.log({ "token": token });
-    getPosts(token)
-      .then((data) => setPosts(data));
+    console.log({ token: token });
+    getPosts(token).then((data) => setPosts(data));
     console.log({ "current my posts": allPosts });
   }, []);
 
   useEffect(() => {
-    setMyPosts(allPosts.filter(p => p.user_id == token));
+    setMyPosts(allPosts.filter((p) => p.user_id == token));
   }, [allPosts, token]);
 
   const handleEdit = (postId) => {
@@ -24,8 +24,10 @@ export const UserPosts = ({ token }) => {
   };
 
   const confirmDeletion = (postId) => {
-    const userConfirmed = window.confirm("Are you sure you want to delete this post?");
-    console.log(userConfirmed)
+    const userConfirmed = window.confirm(
+      "Are you sure you want to delete this post?"
+    );
+    console.log(userConfirmed);
     if (userConfirmed) {
       deletePost(postId)
         .then(() => deletePostTags(postId))
@@ -41,7 +43,7 @@ export const UserPosts = ({ token }) => {
     <div className="section">
       <div className="container">
         <h2 className="title is-3">My Posts</h2>
-        
+
         {myPosts.length === 0 ? (
           <div className="notification is-light">
             <p>No posts found.</p>
@@ -49,42 +51,55 @@ export const UserPosts = ({ token }) => {
         ) : (
           <div className="posts-list">
             {myPosts.map((post) => (
-              <div key={post.id} className="box mb-3 py-2 px-3" style={{ maxWidth: "800px", margin: "0 auto" }}>
+              <div
+                key={post.id}
+                className="box mb-3 py-2 px-3"
+                style={{ maxWidth: "800px", margin: "0 auto" }}
+              >
                 <article className="media">
                   <div className="media-content">
                     <div className="content">
-                      
                       <div className="level mb-2">
                         <div className="level-left">
-                          <Link to={`/allPosts/${post.id}`} className="title is-4">
+                          <Link
+                            to={`/allPosts/${post.id}`}
+                            className="title is-4"
+                          >
                             {post.title}
                           </Link>
                         </div>
                         <div className="level-right">
                           <span className="has-text-grey-light">
-                            {post.publication_date}
+                            {
+                              <HumanDate
+                                date={new Date(
+                                  post.publication_date
+                                ).toLocaleDateString()}
+                              />
+                            }
                           </span>
                         </div>
                       </div>
 
                       <p className="mb-3">{post.content}</p>
-                      
+
                       <section className="mb-4 justify-center">
-                <div style={{ 
-                  maxWidth: "400px", 
-                  maxHeight: "200px", 
-                  overflow: "hidden", 
-                  margin: "0 auto"
-                  }}>
-                  <img 
-                    src={post.image_url} 
-                    alt="Post Cover" 
-                    style={{ width: "100%", objectFit: "cover" }}
-                  />
-                </div>
-              </section>
-                      
-                     
+                        <div
+                          style={{
+                            maxWidth: "400px",
+                            maxHeight: "200px",
+                            overflow: "hidden",
+                            margin: "0 auto",
+                          }}
+                        >
+                          <img
+                            src={post.image_url}
+                            alt="Post Cover"
+                            style={{ width: "100%", objectFit: "cover" }}
+                          />
+                        </div>
+                      </section>
+
                       <div className="level">
                         <div className="level-left">
                           <p className="has-text-weight-medium">
@@ -94,16 +109,16 @@ export const UserPosts = ({ token }) => {
                         <div className="level-right">
                           <div className="field is-grouped">
                             <div className="control">
-                              <button 
-                                className="button is-link is-small" 
+                              <button
+                                className="button is-link is-small"
                                 onClick={() => handleEdit(post.id)}
                               >
                                 Edit
                               </button>
                             </div>
                             <div className="control">
-                              <button 
-                                className="button is-danger is-small" 
+                              <button
+                                className="button is-danger is-small"
                                 onClick={() => confirmDeletion(post.id)}
                               >
                                 Delete
--- src/components/users/UserDetails.js
+++ src/components/users/UserDetails.js
@@ -1,144 +1,175 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
-import { getUserById } from "../../services/userServices"
-import Logo from "../nav/rare.jpeg"
-import { createSubscriptionEntry, getSubscriptions, deleteSubscription } from "../../services/subscriptionServices"
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getUserById } from "../../services/userServices";
+import Logo from "../nav/rare.jpeg";
+import {
+  createSubscriptionEntry,
+  getSubscriptions,
+  deleteSubscription,
+} from "../../services/subscriptionServices";
+import { HumanDate } from "../utils/HumanDate";
 
-export const UserDetails = ({token}) => {
-    const [userDetails, setUserDetails] = useState()
-    const [subscriptions, setSubscriptions] = useState([])
-    const {userId} = useParams()
-    const navigate = useNavigate()
+export const UserDetails = ({ token }) => {
+  const [userDetails, setUserDetails] = useState();
+  const [subscriptions, setSubscriptions] = useState([]);
+  const { userId } = useParams();
+  const navigate = useNavigate();
 
-    const fetchUserDetails = () => {
-        getUserById(Number(userId)).then(user => {
-            setUserDetails(user)
-        })
+  const fetchUserDetails = () => {
+    getUserById(Number(userId)).then((user) => {
+      setUserDetails(user);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserDetails();
+  }, [token]);
+
+  useEffect(() => {
+    getSubscriptions().then((subscription) => {
+      setSubscriptions(subscription);
+    });
+  }, []);
+
+  //evaluates user length and if it is greater than 0, it has found that the current user subscribed to this author
+  const didCurrentUserSubscribe = () => {
+    if (subscriptions) {
+      const userSubscribedToAuthor = subscriptions.find(
+        (userSubscription) =>
+          userSubscription.follower_id === Number(token) &&
+          userSubscription.author_id === userDetails.id
+      );
+      return userSubscribedToAuthor;
     }
+  };
 
-    useEffect(() => {
-        fetchUserDetails()
-    },[token])
+  const handleSubscription = async (event) => {
+    event.preventDefault();
+    const subscriptionEntry = {
+      follower_id: Number(token),
+      author_id: userDetails.id,
+      created_on: new Date(),
+    };
+    createSubscriptionEntry(subscriptionEntry);
+    navigate("/");
+  };
 
-    useEffect(() => {
-        getSubscriptions().then(subscription => {
-            setSubscriptions(subscription)            
-        })
-    },[])
+  const handleUnsubscribe = (event) => {
+    event.preventDefault();
+    const findUserSubscription = subscriptions.find(
+      (subscription) =>
+        subscription.follower_id === Number(token) &&
+        subscription.author_id === userDetails.id
+    );
+    deleteSubscription(findUserSubscription.id);
+  };
 
-    //evaluates user length and if it is greater than 0, it has found that the current user subscribed to this author
-    const didCurrentUserSubscribe = () => {
-        if (subscriptions) {
-            const userSubscribedToAuthor = subscriptions.find((userSubscription) => userSubscription.follower_id === Number(token) && userSubscription.author_id === userDetails.id)
-            return userSubscribedToAuthor
-        }
-    }
+  if (userDetails) {
+    return (
+      <>
+        <section className="section">
+          <div className="container">
+            <div className="columns is-vcentered mb-6">
+              <div className="column is-narrow">
+                <figure className="image is-128x128">
+                  <img
+                    className="is-rounded"
+                    src={
+                      userDetails?.profile_image_url
+                        ? userDetails.profile_image_url
+                        : Logo
+                    }
+                    alt="Profile"
+                  />
+                </figure>
+              </div>
+              <div className="column">
+                <h1 className="title is-3 mb-4">
+                  {userDetails.first_name} {userDetails.last_name}
+                </h1>
+                <p className="subtitle is-5 mb-3">@{userDetails.username}</p>
+              </div>
+              <div className="column is-narrow">
+                {Number(userDetails.id) !== Number(token) && (
+                  <>
+                    {!didCurrentUserSubscribe() && (
+                      <button
+                        className="button is-primary is-rounded"
+                        onClick={handleSubscription}
+                      >
+                        Subscribe
+                      </button>
+                    )}
+                    {!!didCurrentUserSubscribe() && (
+                      <button
+                        className="button is-danger is-rounded"
+                        onClick={handleUnsubscribe}
+                      >
+                        Unsubscribe
+                      </button>
+                    )}
+                  </>
+                )}
+              </div>
+            </div>
 
-    const handleSubscription = async(event) => {
-        event.preventDefault()
-        const subscriptionEntry = {
-            follower_id: Number(token),
-            author_id: userDetails.id,
-            created_on: new Date()
-        }
-        createSubscriptionEntry(subscriptionEntry)
-        navigate("/")
-    }
-    
-    const handleUnsubscribe = (event) => {
-        event.preventDefault()
-        const findUserSubscription = subscriptions.find((subscription) => subscription.follower_id === Number(token) && subscription.author_id === userDetails.id)
-        deleteSubscription(findUserSubscription.id)
-    }
-
-    if(userDetails){
-        return <>
-            <section className="section">
-                <div className="container">
-                    <div className="columns is-vcentered mb-6">
-                        <div className="column is-narrow">
-                            <figure className="image is-128x128">
-                                <img 
-                                    className="is-rounded" 
-                                    src={userDetails?.profile_image_url ? userDetails.profile_image_url : Logo} 
-                                    alt="Profile" 
-                                />
-                            </figure>
-                        </div>
-                        <div className="column">
-                            <h1 className="title is-3 mb-4">{userDetails.first_name} {userDetails.last_name}</h1>
-                            <p className="subtitle is-5 mb-3">@{userDetails.username}</p>
-                        </div>
-                        <div className="column is-narrow">
-                            {Number(userDetails.id) !== Number(token) && (
-                                <>
-                                    {!didCurrentUserSubscribe() && 
-                                        <button 
-                                            className="button is-primary is-rounded" 
-                                            onClick={handleSubscription}
-                                        >
-                                            Subscribe
-                                        </button>
-                                    }
-                                    {!!didCurrentUserSubscribe() && 
-                                        <button 
-                                            className="button is-danger is-rounded" 
-                                            onClick={handleUnsubscribe}
-                                        >
-                                            Unsubscribe
-                                        </button>
-                                    }
-                                </>
-                            )}
-                        </div>
-                    </div>
-                    
-                    <div className="box">
-                        <h3 className="title is-4 mb-4">User Information</h3>
-                        <div className="content">
-                            <div className="field">
-                                <label className="label">Username:</label>
-                                <div className="control">
-                                    <p className="subtitle is-6">{userDetails.username}</p>
-                                </div>
-                            </div>
-                            
-                            <div className="field">
-                                <label className="label">Email:</label>
-                                <div className="control">
-                                    <p className="subtitle is-6">{userDetails.email}</p>
-                                </div>
-                            </div>
-                            
-                            <div className="field">
-                                <label className="label">Join Date:</label>
-                                <div className="control">
-                                    <p className="subtitle is-6">{new Date(userDetails.created_on).toLocaleDateString()}</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div className="mt-5">
-                            <h3 className="title is-5 mb-3">About:</h3>
-                            <div className="content">
-                                <p>{userDetails.bio || "No bio available."}</p>
-                            </div>
-                        </div>
-                        <div className="mt-5">
-                            <h3 className="title is-5 mb-3">Role:</h3>
-                            <div className="content">
-                                <p>{userDetails.is_admin ? "admin" : "author"}</p>
-                            </div>
-                        </div>
-                    </div>
+            <div className="box">
+              <h3 className="title is-4 mb-4">User Information</h3>
+              <div className="content">
+                <div className="field">
+                  <label className="label">Username:</label>
+                  <div className="control">
+                    <p className="subtitle is-6">{userDetails.username}</p>
+                  </div>
                 </div>
-            </section>
-        </>
-    }
-    
-    return <div className="section">
-        <div className="container">
-            <p className="has-text-centered">Loading user details...</p>
-        </div>
+
+                <div className="field">
+                  <label className="label">Email:</label>
+                  <div className="control">
+                    <p className="subtitle is-6">{userDetails.email}</p>
+                  </div>
+                </div>
+
+                <div className="field">
+                  <label className="label">Join Date:</label>
+                  <div className="control">
+                    <p className="subtitle is-6">
+                      {
+                        <HumanDate
+                          date={new Date(
+                            userDetails.created_on
+                          ).toLocaleDateString()}
+                        />
+                      }
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-5">
+                <h3 className="title is-5 mb-3">About:</h3>
+                <div className="content">
+                  <p>{userDetails.bio || "No bio available."}</p>
+                </div>
+              </div>
+              <div className="mt-5">
+                <h3 className="title is-5 mb-3">Role:</h3>
+                <div className="content">
+                  <p>{userDetails.is_admin ? "admin" : "author"}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+
+  return (
+    <div className="section">
+      <div className="container">
+        <p className="has-text-centered">Loading user details...</p>
+      </div>
     </div>
-}
+  );
+};
